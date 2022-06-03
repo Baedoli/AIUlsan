263ac7b3593739ef75da9bf3022dd6b4d42ffe1e
baeseongho <bae9808@gmail.com>
2022-06-03 21:23:04 +0900

    20220603 마지막 정리

--- Study_Basic/edu09.py
+++ Study_Basic/edu09.py
@@ -139,6 +139,8 @@ print('-'*10)
 for i in list1 :
     print(i)
 
+from cmath import pi
+from json import load
 import random
 for i in iter(lambda : random.randint(0,5), 2 ) :
     print(i,end=" ")
@@ -171,3 +173,124 @@ for row in data:
         temp = float(row[3])
 print('가장 추운 날은 ',temp,' 입니다.')
 f.close()
+
+import pickle
+f = open('test.txt','wb')
+data = {1:'python',2:'you need'}
+pickle.dump(data,f)
+f.close()
+
+import pickle
+f = open('test.txt','rb')
+data = pickle.load(f)
+print(data)
+f.close()
+
+import pickle
+list1 = ['a','b','c']
+
+with open('test.wb','wb') as fd :
+    pickle.dump(list1,fd)
+    
+with open('test.wb','rb') as fd :
+    data = pickle.load(fd)
+print(data)
+
+import pickle
+name = 'bae'
+age = 48
+address = 'ulsan pyongdong-3gil 2'
+score = {'kor':90,'eng':99,'math':100}
+
+with open('test.wc','wb') as fd :
+    pickle.dump(name,fd)
+    pickle.dump(age,fd)
+    pickle.dump(address,fd)
+    pickle.dump(score,fd)
+
+import pickle
+with open('test.wc','rb') as fd :
+    name = pickle.load(fd)
+    age = pickle.load(fd)
+    address = pickle.load(fd)
+    score = pickle.load(fd)
+
+print(name)
+print(age)
+print(address)
+print(score)  
+
+import re
+password = input('input password : ')
+
+flag = 0
+while True :
+    if len(password) < 8 :
+        flag = -1
+        break
+    elif not re.search('[a-z]',password) :
+        flag = -1
+        break
+    elif not re.search('[A-Z]',password) :
+        flag = -1
+        break
+    elif not re.search('[0-9]',password) :
+        flag = -1
+        break
+    elif not re.search('[_@$!]',password) :
+        flag = -1
+        break
+    else :
+        flag = 0
+        print('valid password')
+        break
+if flag==-1 :
+    print('invalid password')
+
+import re
+f = open('hello.txt','r')
+for line in f :
+    line = line.strip('\n')
+    if re.search('if', line) :
+        print(line)
+f.close()
+
+try :
+    x = int(input('input number : '))
+    y = 10 / x
+    print(y)
+
+except :
+    print('exception fire !!!!')
+    
+try :
+    x = int(input('input number : '))
+    y = 10 / x
+    print(y)
+
+except ZeroDivisionError:
+    print('exception fire !!!!')
+
+while True :
+    try :
+        n = input('number input : ')
+        n = int(n)
+        break
+    except ValueError :
+        print('정수가 아닙니다.')
+print('정수 입력에 성공 하셨습니다.')
+
+y = [10,20,30]
+try :
+    index, x = map(int, input('인덱스와 나눌 숫자를 입력하세요.').split())
+    print(y[index]/x)
+except (ZeroDivisionError,IndexError) as e :
+    print('exception fire :',e)
+
+y = [10,20,30]
+try :
+    index, x = map(int, input('인덱스와 나눌 숫자를 입력하세요.').split())
+    print(y[index]/x)
+except Exception as e :
+    print('exception fire :',e)
+    
