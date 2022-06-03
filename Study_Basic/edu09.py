4c0e45e3c580c0511447d940025bc810c7f3ddab
baeseongho <bae9808@gmail.com>
2022-06-03 19:45:53 +0900

    20220603 세번째 개선

--- Study_Basic/edu09.py
+++ Study_Basic/edu09.py
@@ -83,4 +83,91 @@ word_list = line.split()
 for word in word_list:
     print(word)
 fd.close()                
-                    
+
+infilename = input('input file name :')
+outfilename = input('output file name :')
+
+ifile = open(infilename,'r',encoding='UTF-8')
+ofile = open(outfilename,'w',encoding='UTF-8')
+
+sum = 0
+count = 0
+
+line = ifile.readline()
+while line != "" :
+    s = int(line)
+    sum += s
+    count += 1
+    line = ifile.readline()
+
+ofile.write('Total Sales Amount = '+str(sum)+'\n')
+ofile.write('Avg Daily Sales Amount = '+str(sum/count))
+
+ifile.close()
+ofile.close()
+
+list1 = [1,2,3]
+i = iter(list1)
+print(next(i))
+print(next(i))
+print(next(i))
+print(next(i))
+
+list1 = [1,2,3,4,5]
+i = iter(list1)
+print(type(i))
+try :
+    while True :
+        print(next(i))
+except :
+    None
+
+# 한번 사용 하고 나면 다시 사용 불가 한게 이터레이터 이다. 
+# 아래 예제에서 보면 한번 사용 후 다시 사용해 보면 사용 할 수가 앖다.    
+list1 = [1,2,3,4,5]
+ia = iter(list1)
+for i in ia :
+    print(i)
+for i in ia :
+    print(i)
+
+list1 = [1,2,3,4,5]
+ia = iter(list1)
+for i in ia :
+    print(i)
+print('-'*10)
+for i in list1 :
+    print(i)
+
+import random
+for i in iter(lambda : random.randint(0,5), 2 ) :
+    print(i,end=" ")
+
+import random
+while True :
+    i = random.randint(0,5)
+    if i==2 :
+        break
+    print(i, end=" ")
+    
+code = iter(['HTML','CSS','JS'])
+for i in range(1,4) :
+    x = next(code)
+    print(x)
+    
+code = iter(['HTML','CSS','JS'])
+for i in range(1,5) :
+    x = next(code,'end')
+    print(x)
+
+import csv
+f = open('weather.csv','r')
+data = csv.reader(f)
+header = next(data)
+print(header)
+temp = 1000
+for row in data:
+    if temp > float(row[3]) :
+        temp = float(row[3])
+print('가장 추운 날은 ',temp,' 입니다.')
+f.close()
