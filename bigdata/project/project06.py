562a7445ebb4626d0c5026982248fa7574eba6de
baeseongho <bae9808@gmail.com>
2022-06-22 21:24:24 +0900

    20220622 마지막

--- bigdata/project/project06.py
+++ bigdata/project/project06.py
@@ -44,3 +44,57 @@ crime_analysis_police[crime_analysis_police['관서명']=='금천서']
 
 crime_analysis_police.to_csv(path+'02. crime_in_Seoul_include_gu_name2.csv',encoding='utf-8-sig')
 crime_analysis_police.head()
+
+crime_analysis_raw = pd.read_csv(path+'02. crime_in_Seoul_include_gu_name2.csv',index_col=0)
+crime_analysis_raw.head()
+
+crime_analysis = pd.pivot_table(crime_analysis_raw,index='구별',aggfunc=np.sum)
+crime_analysis.head()
+
+crime_analysis['강간검거율'] = crime_analysis['강간 검거']/crime_analysis['강간 발생']*100
+crime_analysis['강도검거율'] = crime_analysis['강도 검거']/crime_analysis['강도 발생']*100
+crime_analysis['살인검거율'] = crime_analysis['살인 검거']/crime_analysis['살인 발생']*100
+crime_analysis['절도검거율'] = crime_analysis['절도 검거']/crime_analysis['절도 발생']*100
+crime_analysis['폭력검거율'] = crime_analysis['폭력 검거']/crime_analysis['폭력 발생']*100
+
+del crime_analysis['강간 검거']
+del crime_analysis['강도 검거']
+del crime_analysis['살인 검거']
+del crime_analysis['절도 검거']
+del crime_analysis['폭력 검거']
+
+crime_analysis.head()
+
+con_list = ['강간검거율','강도검거율','살인검거율','절도검거율','폭력검거율']
+for column in con_list :
+    crime_analysis.loc[crime_analysis[column]>100, column] = 100
+    
+crime_analysis.rename(columns={'강간 발생':'강간',
+                               '강도 발생':'강도',
+                               '살인 발생':'살인',
+                               '절도 발생':'절도',
+                               '폭력 발생':'폭력'}, inplace=True)    
+
+from sklearn import preprocessing
+col = ['강간','강도','살인','절도','폭력']
+x = crime_analysis[col].values
+x
+
+min_max_scaler = preprocessing.MinMaxScaler()
+x_scaled = min_max_scaler.fit_transform(x.astype(float))
+x_scaled
+
+
+col2 = ['강간검거율','강도검거율','살인검거율','절도검거율','폭력검거율']
+crime_analysis_norm = pd.DataFrame(x_scaled,columns=col,index=crime_analysis.index)
+crime_analysis_norm.head()
+crime_analysis_norm[col2] = crime_analysis[col2]
+
+result_CCTV = pd.read_csv(path+'01. CCTV_result2.csv',index_col='구별')
+result_CCTV.head()
+
+crime_analysis_norm[['인구수','CCTV']] = result_CCTV[['인구수','소계']]
+crime_analysis_norm['범죄'] = np.sum(crime_analysis_norm[col],axis=1)
+crime_analysis_norm['검거'] = np.sum(crime_analysis_norm[col2],axis=1)
+
+crime_analysis_norm
