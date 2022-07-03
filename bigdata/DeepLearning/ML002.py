5b9d8d821409dab97f012433aa7836255a49c095
baeseongho <bae9808@gmail.com>
2022-07-03 15:58:03 +0900

    20220703

--- bigdata/DeepLearning/ML002.py
+++ bigdata/DeepLearning/ML002.py
@@ -126,3 +126,160 @@ sgd.score(x_test,y_test)
 
 print(sgd.predict(x_test[0:10]))
 
+class SingleLayer1:
+
+    def __init__(self, learning_rate=0.1, l1=0, l2=0):
+        self.w = None
+        self.b = None
+        self.losses = []
+        self.val_losses = []
+        self.w_history = []
+        self.lr = learning_rate
+        self.l1 = l1
+        self.l2 = l2
+
+    def forpass(self, x):
+        z = np.sum(x * self.w) + self.b    # 직선 방정식을 계산합니다
+        return z
+
+    def backprop(self, x, err):
+        w_grad = x * err          # 가중치에 대한 그래디언트를 계산합니다
+        b_grad = 1 * err    # 절편에 대한 그래디언트를 계산합니다
+        return w_grad, b_grad
+
+    def activation(self, z):
+        z = np.clip(z, -100, None) # 안전한 np.exp() 계산을 위해
+        a = 1 / (1 + np.exp(-z))  # 시그모이드 계산
+        return a
+
+    def fit(self, x, y, epochs=100, x_val=None, y_val=None):
+        self.w = np.ones(x.shape[1])               # 가중치를 초기화합니다.
+        self.b = 0                                 # 절편을 초기화합니다.
+        self.w_history.append(self.w.copy())       # 가중치를 기록합니다.
+        np.random.seed(42)                         # 랜덤 시드를 지정합니다.
+        for i in range(epochs):                    # epochs만큼 반복합니다.
+            loss = 0
+            # 인덱스를 섞습니다
+            indexes = np.random.permutation(np.arange(len(x)))
+            for i in indexes:                      # 모든 샘플에 대해 반복합니다
+                z = self.forpass(x[i])             # 정방향 계산
+                a = self.activation(z)             # 활성화 함수 적용
+                err = -(y[i] - a)                  # 오차 계산
+                w_grad, b_grad = self.backprop(x[i], err) # 역방향 계산
+                # 그래디언트에서 페널티 항의 미분 값을 더합니다
+                w_grad += self.l1 * np.sign(self.w) + self.l2 * self.w
+                self.w -= self.lr * w_grad         # 가중치 업데이트
+                self.b -= b_grad                   # 절편 업데이트
+                # 가중치를 기록합니다.
+                self.w_history.append(self.w.copy())
+                # 안전한 로그 계산을 위해 클리핑한 후 손실을 누적합니다
+                a = np.clip(a, 1e-10, 1-1e-10)
+                loss += -(y[i]*np.log(a)+(1-y[i])*np.log(1-a))
+            # 에포크마다 평균 손실을 저장합니다
+            self.losses.append(loss/len(y) + self.reg_loss())
+            # 검증 세트에 대한 손실을 계산합니다
+            self.update_val_loss(x_val, y_val)
+
+    def predict(self, x):
+        z = [self.forpass(x_i) for x_i in x]     # 정방향 계산
+        return np.array(z) >= 0                   # 스텝 함수 적용
+
+    def score(self, x, y):
+        return np.mean(self.predict(x) == y)
+
+    def reg_loss(self):
+        return self.l1 * np.sum(np.abs(self.w)) + self.l2 / 2 * np.sum(self.w**2)
+
+    def update_val_loss(self, x_val, y_val):
+        if x_val is None:
+            return
+        val_loss = 0
+        for i in range(len(x_val)):
+            z = self.forpass(x_val[i])     # 정방향 계산
+            a = self.activation(z)         # 활성화 함수 적용
+            a = np.clip(a, 1e-10, 1-1e-10)
+            val_loss += -(y_val[i]*np.log(a)+(1-y_val[i])*np.log(1-a))
+        self.val_losses.append(val_loss/len(y_val) + self.reg_loss())
+
+layer1 = SingleLayer()
+layer1.fit(x_train, y_train)
+layer1.score(x_val, y_val)class SingleLayer:
+
+class SingleLayer1:
+    def __init__(self, learning_rate=0.1, l1=0, l2=0):
+        self.w = None
+        self.b = None
+        self.losses = []
+        self.val_losses = []
+        self.w_history = []
+        self.lr = learning_rate
+        self.l1 = l1
+        self.l2 = l2
+
+    def forpass(self, x):
+        z = np.sum(x * self.w) + self.b    # 직선 방정식을 계산합니다
+        return z
+
+    def backprop(self, x, err):
+        w_grad = x * err          # 가중치에 대한 그래디언트를 계산합니다
+        b_grad = 1 * err    # 절편에 대한 그래디언트를 계산합니다
+        return w_grad, b_grad
+
+    def activation(self, z):
+        z = np.clip(z, -100, None) # 안전한 np.exp() 계산을 위해
+        a = 1 / (1 + np.exp(-z))  # 시그모이드 계산
+        return a
+
+    def fit(self, x, y, epochs=100, x_val=None, y_val=None):
+        self.w = np.ones(x.shape[1])               # 가중치를 초기화합니다.
+        self.b = 0                                 # 절편을 초기화합니다.
+        self.w_history.append(self.w.copy())       # 가중치를 기록합니다.
+        np.random.seed(42)                         # 랜덤 시드를 지정합니다.
+        for i in range(epochs):                    # epochs만큼 반복합니다.
+            loss = 0
+            # 인덱스를 섞습니다
+            indexes = np.random.permutation(np.arange(len(x)))
+            for i in indexes:                      # 모든 샘플에 대해 반복합니다
+                z = self.forpass(x[i])             # 정방향 계산
+                a = self.activation(z)             # 활성화 함수 적용
+                err = -(y[i] - a)                  # 오차 계산
+                w_grad, b_grad = self.backprop(x[i], err) # 역방향 계산
+                # 그래디언트에서 페널티 항의 미분 값을 더합니다
+                w_grad += self.l1 * np.sign(self.w) + self.l2 * self.w
+                self.w -= self.lr * w_grad         # 가중치 업데이트
+                self.b -= b_grad                   # 절편 업데이트
+                # 가중치를 기록합니다.
+                self.w_history.append(self.w.copy())
+                # 안전한 로그 계산을 위해 클리핑한 후 손실을 누적합니다
+                a = np.clip(a, 1e-10, 1-1e-10)
+                loss += -(y[i]*np.log(a)+(1-y[i])*np.log(1-a))
+            # 에포크마다 평균 손실을 저장합니다
+            self.losses.append(loss/len(y) + self.reg_loss())
+            # 검증 세트에 대한 손실을 계산합니다
+            self.update_val_loss(x_val, y_val)
+
+    def predict(self, x):
+        z = [self.forpass(x_i) for x_i in x]     # 정방향 계산
+        return np.array(z) >= 0                   # 스텝 함수 적용
+
+    def score(self, x, y):
+        return np.mean(self.predict(x) == y)
+
+    def reg_loss(self):
+        return self.l1 * np.sum(np.abs(self.w)) + self.l2 / 2 * np.sum(self.w**2)
+
+    def update_val_loss(self, x_val, y_val):
+        if x_val is None:
+            return
+        val_loss = 0
+        for i in range(len(x_val)):
+            z = self.forpass(x_val[i])     # 정방향 계산
+            a = self.activation(z)         # 활성화 함수 적용
+            a = np.clip(a, 1e-10, 1-1e-10)
+            val_loss += -(y_val[i]*np.log(a)+(1-y_val[i])*np.log(1-a))
+        self.val_losses.append(val_loss/len(y_val) + self.reg_loss())
+
+layer1 = SingleLayer1()
+layer1.fit(x_train, y_train)
+layer1.score(x_val, y_val)
+
