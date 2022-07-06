1cd868f52ede7b609b651cff78de509aea09893c
baeseongho <bae9808@gmail.com>
2022-07-06 20:04:51 +0900

    20220706 수업 두번째 ..

--- bigdata/DeepLearning/ML005.py
+++ bigdata/DeepLearning/ML005.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 bream_length = [25.4, 26.3, 26.5, 29.0, 29.0, 29.7, 29.7, 30.0, 30.0, 30.7, 31.0, 31.0, 31.5, 32.0, 32.0, 32.0, 33.0, 33.0, 33.5, 33.5, 34.0, 34.0, 34.5, 35.0, 35.0, 35.0, 35.0, 36.0, 36.0, 37.0, 38.5, 38.5, 39.5, 41.0, 41.0]
 bream_weight = [242.0, 290.0, 340.0, 363.0, 430.0, 450.0, 500.0, 390.0, 450.0, 500.0, 475.0, 500.0, 500.0, 340.0, 600.0, 600.0, 700.0, 700.0, 610.0, 650.0, 575.0, 685.0, 620.0, 680.0, 700.0, 725.0, 720.0, 714.0, 850.0, 1000.0, 920.0, 955.0, 925.0, 975.0, 950.0]
@@ -67,4 +68,102 @@ plt.show()
 
 kn.fit(train_input,train_target)
 kn.score(test_input,test_target)
+
+print(kn.predict(test_input))
+print(test_target)
+
+fish_length = [25.4, 26.3, 26.5, 29.0, 29.0, 29.7, 29.7, 30.0, 30.0, 30.7, 31.0, 31.0,
+               31.5, 32.0, 32.0, 32.0, 33.0, 33.0, 33.5, 33.5, 34.0, 34.0, 34.5, 35.0,
+               35.0, 35.0, 35.0, 36.0, 36.0, 37.0, 38.5, 38.5, 39.5, 41.0, 41.0, 9.8,
+               10.5, 10.6, 11.0, 11.2, 11.3, 11.8, 11.8, 12.0, 12.2, 12.4, 13.0, 14.3, 15.0]
+fish_weight = [242.0, 290.0, 340.0, 363.0, 430.0, 450.0, 500.0, 390.0, 450.0, 500.0, 475.0, 500.0,
+               500.0, 340.0, 600.0, 600.0, 700.0, 700.0, 610.0, 650.0, 575.0, 685.0, 620.0, 680.0,
+               700.0, 725.0, 720.0, 714.0, 850.0, 1000.0, 920.0, 955.0, 925.0, 975.0, 950.0, 6.7,
+               7.5, 7.0, 9.7, 9.8, 8.7, 10.0, 9.9, 9.8, 12.2, 13.4, 12.2, 19.7, 19.9]
+
+fish_data = np.column_stack((fish_length,fish_weight))
+fish_target = np.concatenate((np.ones(35), np.zeros(14)))
+
+train_input, test_input, train_target, test_target = train_test_split(fish_data, fish_target, random_state=42)
+
+print(train_input.shape, test_input.shape)
+print(train_target.shape, test_target.shape)
+
+# 무작위로 stratify 값을 주기 않고 지정 하면 0 이 3개 본포 ,,,
+print(test_target)
+
+train_input, test_input, train_target, test_target = train_test_split(fish_data, fish_target,stratify=fish_target, random_state=42)
+
+# finsh_target 을 설정하면 0 의 비율을 target 의 비율을 가져와서 설정 하므로
+# 주지 않았을때 보다 비율이 더 정확해져서 score 가 더 좋게 나온다..
+print(test_target)
+
+kn.fit(train_input,train_target)
+kn.score(test_input,test_target)
+
+print(kn.predict([[25,150]]))
+
+plt.scatter(train_input[:,0], train_input[:,1])
+plt.scatter(25,150,marker='^')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+# 추축값 의 거리와 인텍스 표시 ..
+distances, indexes = kn.kneighbors([[25, 150]])
+print(distances)
+print(indexes)
+
+plt.scatter(train_input[:,0], train_input[:,1])
+plt.scatter(25,150,marker='^')
+plt.scatter(train_input[indexes,0],train_input[indexes,1],marker='D')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+# x 값을 일렬로 표시 되는거 보니 x 는 거의 영향을 미치지 못한다.
+# 정규화 필요 ..
+plt.scatter(train_input[:,0],train_input[:,1])
+plt.scatter(25,150,marker='^')
+plt.scatter(train_input[indexes,0],train_input[indexes,1],marker='D')
+plt.xlim((0,1000))
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+mean = np.mean(train_input,axis=0)
+std = np.std(train_input,axis=0)
+
+train_scaled = (train_input-mean)/std
+
+# 25,150 도 전처리 해줘야 동일 선상에 비교가 가능 하다 ..
+plt.scatter(train_scaled[:,0], train_scaled[:,1])
+plt.scatter(25,150,marker='^')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+new = ([25,150] - mean)/std
+
+# 25,150 을 전처리 하고 나서의 비율 ...
+plt.scatter(train_scaled[:,0], train_scaled[:,1])
+plt.scatter(new[0], new[1],marker='^')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+kn.fit(train_scaled,train_target)
+test_scaled = (test_input - mean) / std
+
+distances, indexes = kn.kneighbors([new])
+
+plt.scatter(train_scaled[:,0],train_scaled[:,1])
+plt.scatter(new[0],new[1],marker='^')
+plt.scatter(train_scaled[indexes,0],train_scaled[indexes,1],
+            marker='D')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+
 
