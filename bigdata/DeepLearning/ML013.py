7f08579138126d5b18aeb44c58be66171069f0d1
baeseongho <bae9808@gmail.com>
2022-07-12 20:38:58 +0900

    20220712 수업 두번째 ..

--- bigdata/DeepLearning/ML013.py
+++ bigdata/DeepLearning/ML013.py
@@ -1,1 +1,99 @@
-from
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_validate
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import cross_validate
+from sklearn.linear_model import SGDClassifier
+
+
+
+(train_input, train_target), (test_input, test_target) = keras.datasets.fashion_mnist.load_data()
+
+print(train_input.shape, train_target.shape)
+print(test_input.shape, test_target.shape)
+
+fig, axs = plt.subplots(1, 10, figsize=(10,10))
+for i in range(10):
+    axs[i].imshow(train_input[i],cmap='gray_r')
+    axs[i].axis('off')
+plt.show()
+
+print([train_target[i] for i in range(10)])
+print(np.unique(train_target, return_counts=True))
+
+train_scaled = train_input/255.0
+train_scaled = train_scaled.reshape(-1, 28*28)
+
+sc = SGDClassifier(loss='log', random_state=42)
+scores = cross_validate(sc, train_scaled, train_target, n_jobs=-1)
+print(np.mean(scores['test_score']))
+
+sc = SGDClassifier(loss='log', max_iter=15, random_state=42)
+scores = cross_validate(sc, train_scaled, train_target, n_jobs=-1)
+print(np.mean(scores['test_score']))
+
+train_scaled, val_scaled, train_target, val_target=train_test_split(train_scaled, train_target, test_size=0.2, random_state=42)
+# 784 는 28*28 개의 속성이라서 ..
+dense = keras.layers.Dense(10, activation='softmax', input_shape=(784,))
+model = keras.Sequential(dense)
+model.compile(loss='sparse_categorical_crossentropy', metrics='accuracy')
+print(train_target[:10])
+model.fit(train_scaled, train_target, epochs=5)
+model.evaluate(val_scaled, val_target)
+
+# Case 1 : 일반적인 2개의 층으로 구성 ..
+(train_input, train_target),(test_input, test_target) = keras.datasets.fashion_mnist.load_data()
+train_scaled = train_input/255.0
+train_scaled = train_scaled.reshape(-1,28*28)
+train_scaled, val_scaled, train_target, val_target = train_test_split(train_scaled, train_target, test_size=0.2, random_state=42)
+
+dense1 = keras.layers.Dense(100, activation='sigmoid', input_shape=(784,))
+dense2 = keras.layers.Dense(10, activation='softmax')
+
+model = keras.Sequential([dense1,dense2])
+model.summary()
+
+# case 2
+model = keras.Sequential([
+    keras.layers.Dense(100,activation='sigmoid', input_shape=(784,), name='hiddeh'),
+    keras.layers.Dense(10,activation='softmax',name='output')
+], name='패션 MNST 모델')
+model.summary()
+
+# case 3
+model = keras.Sequential()
+model.add(keras.layers.Dense(100,activation='sigmoid', input_shape=(784,)))
+model.add(keras.layers.Dense(10,activation='softmax'))
+model.summary()
+
+model = keras.Sequential()
+model.add(keras.layers.Flatten(input_shape=(28,28)))
+model.add(keras.layers.Dense(100,activation='relu'))
+model.add(keras.layers.Dense(10,activation='softmax'))
+model.summary()
+
+model=keras.Sequential()
+model.add(keras.layers.Flatten(input_shape=(28, 28)))
+model.add(keras.layers.Dense(100, activation='relu'))
+model.add(keras.layers.Dense(10, activation='softmax'))
+model.summary()
+
+from sklearn.model_selection import train_test_split
+train_scaled = train_input / 255.0
+train_scaled, val_scaled, train_target, val_target = train_test_split(train_scaled, train_target, test_size=0.2, random_state=42)
+
+model.compile(loss='sparse_categorical_crossentropy', metrics='accuracy')
+model.fit(train_scaled, train_target, epochs=5)
+model.evaluate(val_scaled, val_target)
+
+model=keras.Sequential()
+model.add(keras.layers.Flatten(input_shape=(28,28)))
+model.add(keras.layers.Dense(100, activation='relu'))
+model.add(keras.layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',metrics='accuracy')
+model.fit(train_scaled, train_target, epochs=5)
+model.evaluate(val_scaled, val_target)
+
