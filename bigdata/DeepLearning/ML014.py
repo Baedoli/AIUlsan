0808060528ed60140fde3b5e9459a5287bde1877
baeseongho <bae9808@gmail.com>
2022-07-13 21:20:15 +0900

    20220713 수업 마지막 ..

--- bigdata/DeepLearning/ML014.py
+++ bigdata/DeepLearning/ML014.py
@@ -1,6 +1,9 @@
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+import os
+import numpy as np
+
 
 (train_input, train_target),(test_input, test_target) = keras.datasets.fashion_mnist.load_data()
 train_scaled = train_input/255.0
@@ -84,3 +87,39 @@ history=model.fit(train_scaled, train_target, epochs=10, verbose=0, validation_d
 model.save_weights('model-weights.h5')
 model.save('model-whole.h5')
 
+path = os.getcwd()
+path = path+'/'
+
+model = model_fn(keras.layers.Dropout(0,3))
+model.load_weights(path+'model-weights.h5')
+
+val_labels = np.argmax(model.predict(val_scaled), axis=-1)
+print(np.mean(val_labels == val_target))
+
+model = keras.models.load_model(path+'model-whole.h5')
+model.evaluate(val_scaled,val_target)
+
+model = model_fn(keras.layers.Dropout(0.3))
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',metrics='accuracy')
+checkpoint_cb=keras.callbacks.ModelCheckpoint('best-model.h5')
+model.fit(train_scaled, train_target,epochs=20,verbose=0, validation_data=(val_scaled, val_target), callbacks=[checkpoint_cb])
+
+model = keras.models.load_model('best-model.h5')
+model.evaluate(val_scaled, val_target)
+
+model=model_fn(keras.layers.Dropout(0.3))
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics='accuracy')
+checkpoint_cb =keras.callbacks.EarlyStopping('best-model.h5')
+early_stopping_cb=keras.callbacks.EarlyStopping(patience=2, restore_best_weights=True)
+histoy=model.fit(train_scaled, train_target, epochs=20, verbose=0, validation_data=(val_scaled, val_target), callbacks=[checkpoint_cb, early_stopping_cb])
+
+print(early_stopping_cb.stopped_epoch)
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend(['train','val'])
+plt.show()
+
+model.evaluate(val_scaled, val_target)
