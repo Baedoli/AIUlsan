ea0831d357469906e4232ebb665ff930b7cc56c8
baeseongho <bae9808@gmail.com>
2022-07-13 18:06:50 +0900

    20220713 수업 전 ..

--- bigdata/DeepLearning/ML014.py
+++ bigdata/DeepLearning/ML014.py
@@ -1,5 +1,6 @@
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 (train_input, train_target),(test_input, test_target) = keras.datasets.fashion_mnist.load_data()
 train_scaled = train_input/255.0
@@ -16,4 +17,70 @@ def model_fn(a_layer=None):
 
 model=model_fn()
 model.summary()
+
+model.compile(loss='sparse_categorical_crossentropy', metrics='accuracy')
+history = model.fit(train_scaled, train_target, epochs=5, verbose=0)
+print(history.history.keys())
+
+plt.plot(history.history['loss'])
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.show()
+
+plt.plot(history.history['accuracy'])
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.show()
+
+model=model_fn()
+model.compile(loss='sparse_categorical_crossentropy',metrics='accuracy')
+history=model.fit(train_scaled, train_target, epochs=20, verbose=0)
+plt.plot(history.history['loss'])
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.show()
+
+model = model_fn()
+model.compile(loss='sparse_categorical_crossentropy',metrics='accuracy')
+history=model.fit(train_scaled, train_target, epochs=20, verbose=0,validation_data=(val_scaled, val_target))
+
+print(history.history.keys())
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend(['train','val'])
+plt.show()
+
+# 옵티마이져 ...
+model=model_fn()
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',metrics='accuracy')
+history=model.fit(train_scaled, train_target, epochs=20, verbose=0,validation_data=(val_scaled, val_target))
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend(['train','val'])
+plt.show()
+
+# 은닉층의 뉴럴을 임의로 수행 하지 않게 하여 많이 학습하는 과대 적합을 피할 수 있다...
+model=model_fn(keras.layers.Dropout(0.3))
+model.summary()
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',metrics='accuracy')
+history=model.fit(train_scaled, train_target, epochs=20, verbose=0,validation_data=(val_scaled, val_target))
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend(['train','val'])
+plt.show()
+
+model=model_fn(keras.layers.Dropout(0.3))
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',metrics='accuracy')
+history=model.fit(train_scaled, train_target, epochs=10, verbose=0, validation_data=(val_scaled, val_target))
+model.save_weights('model-weights.h5')
+model.save('model-whole.h5')
 
